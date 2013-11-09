3724b2889537ba78b06082238fdfd28115148056
sean <sean@sean-ThinkPad-W520.(none)>
2013-11-08 20:49:56 -0500

    removed breaking code

--- website/php/index.php
+++ website/php/index.php
@@ -2,8 +2,10 @@
   require_once $_SERVER["DOCUMENT_ROOT"]."/lib/custom/models/user.php";
   require_once $_SERVER["DOCUMENT_ROOT"]."/lib/custom/models/relationship.php";
 
-  $id = $_GET["id"];
-  $expression = new Expression("id", "=", $id);
-  $user = Users::read($expression);
-  $url = $user->url;
+  /*** EXAMPLE OF A READ ***
+    $id = $_GET["id"];
+    $expression = new Expression("id", "=", $id);
+    $user = Users::read($expression);
+    $url = $user->url;
+  **************************/
 ?>
