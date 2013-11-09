69f0e93b4d39d62b57159ad4a749c460f3a787ed
silicaRich <nicole.flokos@gmail.com>
2013-11-09 06:20:40 -0500

    registration.zzzz

--- www/js/registration.js
+++ www/js/registration.js
@@ -1,5 +1,16 @@
 $(document).ready(function () {
 	$("#submit").click(fuction(){
-	
+
+        var $defer = $.ajax({
+            url: 'http://' + server + '/api/saveUser.php?first_name'+first_name+'&last_name'+last_name+'&password'+password+'&email='+email+'&company'+company+'&position'+position+'&address'+address+'&website'+url+'&phone'+phone+'&fax'+fax+'&description'+description';
+            type: 'POST',
+            dataType: 'jsonp',
+            success: function (data) {
+                window.location.href="main.html?id="+data[0].id;
+            },
+            error: function (data) {
+                //console.log('error');
+            }
+        });
 	});
 });
